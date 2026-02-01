8525ee7e77ef930c4ddc9936ad7bc0fddfc9b1a9
ElizavetaTelyatnikova <ttelatnikova1@yandex.ru>
2026-02-01 20:25:43 +0300

    Lists in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -1,27 +1,45 @@
-val rockPlanets = arrayOf<String>("Mercury","Venus","Earth","Mars")
-val gasPlanets = arrayOf("Jupiter","Saturn","Uranus","Neptune")
-val solarSystem = rockPlanets + gasPlanets
-val newSolarSystem = arrayOf(
-    "Mercury",
-    "Venus",
-    "Earth",
-    "Mars",
-    "Jupiter",
-    "Saturn",
-    "Uranus",
-    "Neptune",
-    "Pluto"
-)
+//val rockPlanets = arrayOf<String>("Mercury","Venus","Earth","Mars")
+//val gasPlanets = arrayOf("Jupiter","Saturn","Uranus","Neptune")
+//val solarSystem = rockPlanets + gasPlanets
+//val newSolarSystem = arrayOf(
+//    "Mercury",
+//    "Venus",
+//    "Earth",
+//    "Mars",
+//    "Jupiter",
+//    "Saturn",
+//    "Uranus",
+//    "Neptune",
+//    "Pluto"
+//)
 fun main() {
-    println(solarSystem[0])
-    println(solarSystem[1])
+//    println(solarSystem[0])
+//    println(solarSystem[1])
+//    println(solarSystem[2])
+//    println(solarSystem[3])
+//    println(solarSystem[4])
+//    println(solarSystem[5])
+//    println(solarSystem[6])
+//    println(solarSystem[7])
+//    solarSystem[3] = "Little Earth"
+//    println(solarSystem[3])
+//    println(newSolarSystem[8])
+    val solarSystem = mutableListOf("Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune")
+    println(solarSystem.size)
     println(solarSystem[2])
+    println(solarSystem.get(3))
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
+    solarSystem.add("Pluto")
+    solarSystem.add(3,"Theia")
+    for (planet in solarSystem){
+        println(planet)
+    }
+    solarSystem[3] = "Future Moon"
     println(solarSystem[3])
-    println(solarSystem[4])
-    println(solarSystem[5])
-    println(solarSystem[6])
-    println(solarSystem[7])
-    solarSystem[3] = "Little Earth"
-    println(solarSystem[3])
-    println(newSolarSystem[8])
+    println(solarSystem[9])
+    solarSystem.removeAt(9)
+    solarSystem.remove("Future Moon")
+    println(solarSystem.contains("Pluto"))
+    println("Future Moon" in solarSystem)
 }
